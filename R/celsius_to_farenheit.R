e27a7e0a13d85961346d7c6b1cf9a3c335b024e8
devopsuser94 <70208899+devopsuser94@users.noreply.github.com>
2021-09-18 00:23:50 +0530

    change logic in celsius_to_ferenheit
    change logic in celsius_to_ferenheit

--- R/celsius_to_farenheit.R
+++ R/celsius_to_farenheit.R
@@ -8,6 +8,11 @@
 #' @examples
 #' Celsius_to_Fahrenheit(0) # results in 32F
 Celsius_to_Fahrenheit <- function(temp_C) {
+  if(is.numeric(temp_C)){
   temp_F  <-  9*temp_C/5 + 32
   return(temp_F)
+  }
+  else{
+    print("Error! "+temp_C+"is not a numeric")
+  }
 }
